7613e5cf055cc1e1b698ad5a82b3965a5bd96312
wooram <zzzwwwrrr66@gmail.com>
2023-12-09 23:26:35 +0900

    removeDuplicates

--- src/iter-6(~2023-12-31)/80. Remove Duplicates from Sorted Array II/p.js
+++ src/iter-6(~2023-12-31)/80. Remove Duplicates from Sorted Array II/p.js
@@ -3,28 +3,11 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  // sliding window
-  // left and right
-  // hash map
-  const dict = new Map();
-  let left = 0;
-  let right = nums.length - 1;
-  while (left <= right && left < nums.length) {
-    if (!dict.has(nums[left])) {
-      dict.set(nums[left], 1);
-    } else {
-      dict.set(nums[left], dict.get(nums[left]) + 1);
+  for (let i = nums.length - 1; i >= 0; i--) {
+    const prev = nums[i - 2];
+    if (prev === nums[i]) {
+      nums.splice(i, 1);
     }
-
-    if (dict.get(nums[left]) >= 3) {
-      const tmp = nums[right];
-      nums.pop();
-      right--;
-      nums[left] = nums[right];
-      nums[right] = tmp;
-    }
-
-    left++;
   }
 };
 
