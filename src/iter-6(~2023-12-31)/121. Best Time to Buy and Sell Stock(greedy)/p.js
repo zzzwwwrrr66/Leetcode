7ccaf389af10816a69af32b1028634a9a0b6ec0c
wooram <zzzwwwrrr66@gmail.com>
2023-12-15 00:48:35 +0900

    update: greedy

---
+++ src/iter-6(~2023-12-31)/121. Best Time to Buy and Sell Stock(greedy)/p.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  // base case
+  if (prices.length === 1) {
+    return 0;
+  }
+  let result = 0;
+  let minNum = prices[0];
+
+  for (const price of prices) {
+    if (minNum > price) {
+      minNum = price;
+    } else {
+      result = Math.max(result, price - minNum);
+    }
+  }
+
+  return result;
+};
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
