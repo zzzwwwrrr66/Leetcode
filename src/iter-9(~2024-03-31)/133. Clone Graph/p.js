cd50ee842fd6f16a0c005ffdab1e06083c03187f
wooram <zzzwwwrrr66@gmail.com>
2024-03-14 01:06:17 +0900

    graph new Node

---
+++ src/iter-9(~2024-03-31)/133. Clone Graph/p.js
@@ -0,0 +1,15 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function (node) {};
+
+// node 에대한 graph 탐색을 하면서 새로운 클론 노드그래프를 작성한다
